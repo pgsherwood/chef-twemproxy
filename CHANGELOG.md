2a0687d0338ab65a897d3029378aeb991b7d5833
Greg Sherwood <pgscode@gmail.com>
2014-06-21 14:16:28 -0500

    Add files after 'berks cookbook ...' creation and update metadata.rb to fill in placeholders.

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# 0.1.0
+
+Initial release of twemproxy
+
+* Enhancements
+  * an enhancement
+
+* Bug Fixes
+  * a bug fix
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'twemproxy'
+maintainer       'Greg Sherwood'
+maintainer_email 'pgscode@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures twemproxy'
+long_description 'Installs/Configures twemproxy'
+version          '0.1.0'
+
+depends 'apt'
